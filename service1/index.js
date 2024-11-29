39dcfbf0f906b3dfcdd353429abc9034e6044dd2
Shobhit Patkar <shobhitpatkar2000@gmail.com>
2024-11-29 17:30:34 +0000

    first commit

---
+++ service1/index.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+var express = require('express');
+var app = express();
+
+const PORT = process.env.PORT
+const TEXT = process.env.TEXT;
+const NODE1_URL= process.env.NODE1_URL;
+const NODE2_URL= process.env.NODE2_URL;
+const NODE3_URL= process.env.NODE3_URL;
+
+app.get('/test1', function (req, res) {
+  res.send(`listening on Port: ${PORT} \n${TEXT}`);
+});
+
+
+app.get('/test2', function (req, res) {
+  axios.get(`${NODE2_URL}/test2`) 
+  .then(response => {
+    res.send(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
+
+
+
+app.get('/test3', function (req, res) {
+  axios.get(`${NODE3_URL}/test3`)
+  .then(response => {
+    res.send(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
+
+
+
+app.listen(PORT, function () {
+  console.log(`You are listening on port ${PORT}, given text - ${TEXT}` );
+});
