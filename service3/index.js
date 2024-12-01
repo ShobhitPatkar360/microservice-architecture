2830306f93ce1dc473d722bfdb5520d14cccb09f
Shobhit Patkar <66373080+ShobhitPatkar360@users.noreply.github.com>
2024-12-01 19:40:26 +0530

    Update index.js

--- service3/index.js
+++ service3/index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 var express = require('express');
 var app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT
 const TEXT = process.env.TEXT;
@@ -8,6 +9,7 @@ const NODE1_URL= process.env.NODE1_URL;
 const NODE2_URL= process.env.NODE2_URL;
 const NODE3_URL= process.env.NODE3_URL;
 
+app.use(cors()); 
 app.get('/test3', function (req, res) {
   res.send(`listening on Port: ${PORT}  ||  ${TEXT}`);
 });
