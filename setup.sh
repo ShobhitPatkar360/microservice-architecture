a012d49ca43ac1b4fd6c340091f4689afd9504ed
Shobhit Patkar <shobhit.patkar@sosimtech.com>
2025-01-19 19:30:32 +0530

    script added

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ docker build -t image2 .
 cd ./../service3/
 docker build -t image3 .
 history
-docker run -d --name container1 -p 3001:3001 image1 
-docker run -d --name container2 -p 3002:3002 image2 
-docker run -d --name container3 -p 3003:3003 image3 
+docker network create new_network
+docker run -d --name container1 --network new_network -p 3001:3001 image1 
+docker run -d --name container2 --network new_network -p 3002:3002 image2 
+docker run -d --name container3 --network new_network -p 3003:3003 image3 
